7efcbab021c56e51054fc6a86c62a714d09a3d9c
Assaf <assafpro0@gmail.com>
2024-11-19 22:23:26 +0200

    commited!

---
+++ Queue.cpp
@@ -0,0 +1,70 @@
+#include "Queue.h"
+#include <iostream>
+
+void initQueue(Queue* q, unsigned int size)
+{
+	q->_elements = new int[size];
+	q->_maxSize = size;
+}
+
+void cleanQueue(Queue* q)
+{
+	free(q->_elements);
+	q->_elements = NULL;
+	q->_maxSize = 0;
+}
+
+void enqueue(Queue* q, unsigned int newValue)
+{
+	int i = 0;
+	for (i = 0; i < q->_maxSize; i++)
+	{
+		if (q->_elements[i] ==  NULL)
+		{
+			q->_elements[i] = newValue;
+			i = q->_maxSize;
+		}
+	}
+}
+
+int dequeue(Queue* q)
+{
+	int temp = -1;
+	int i = 0;
+	for (i = 0; i < q->_maxSize; i++)
+	{
+		if (q->_elements[i] != NULL)
+		{
+			temp = q->_elements[i];
+			q->_elements[i] = NULL;
+			i = q->_maxSize;
+		}
+	}
+	return temp;
+}
+
+bool isEmpty(Queue* s)
+{
+	int i = 0;
+	for (i = 0; i < s->_maxSize; i++)
+	{
+		if (s->_elements[i] != NULL)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool isFull(Queue* s)
+{
+	int i = 0;
+	for (i = 0; i < s->_maxSize; i++)
+	{
+		if (s->_elements[i] == NULL)
+		{
+			return true;
+		}
+	}
+	return false;
+}
